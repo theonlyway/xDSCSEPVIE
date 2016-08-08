62884c85a88dda743ec23310339e652871674eda
Anthony <anthonywaye@gmail.com>
2016-08-08 19:49:27 +1000

    Updating pester

--- Tests/xDSCSEPVIE.Tests.ps1
+++ Tests/xDSCSEPVIE.Tests.ps1
@@ -1,19 +1,58 @@
 Import-Module -Name .\DSCResources\xDSCSEPVIE\xDSCSEPVIE.psm1
 
-InModuleScope -ModuleName xDSCSEPVIE -ScriptBlock {
-  $VIELocation = 'C:\Temp\vie.exe'
+$Global:DSCModuleName      = 'xDSCSEPVIE' 
+$Global:DSCResourceName    = 'xDSCSEPVIE'
 
-  Describe -Name 'Testing if functions return correct objects' -Fixture {
-    Mock -CommandName Test-Path -MockWith {
-      {
-        return $true
-      }
-    }
+InModuleScope -ModuleName xDSCSEPVIE -ScriptBlock {
+  $VIELocation = 'C:\Temp\doesntmatter.exe'
+
+  $global:mockedVolume = [pscustomobject] @{
+    FileSystemLabel = 'myLabel'
+    DriveLetter     = 'C'
+  }
+  $global:mockedCSVSuccess = [pscustomobject] @{
+    DriveLetter = 'C'
+    DateScanned = (Get-Date -Format dd/MM/yyyy)
+  }
+  $global:mockedCSVFailure = @()
+  $global:mockedCSVFailure += [pscustomobject] @{
+    DriveLetter = 'C'
+    DateScanned = (Get-Date -Format dd/MM/yyyy)
+  }
+  $global:mockedCSVFailure += [pscustomobject] @{
+    DriveLetter = 'D'
+    DateScanned = (Get-Date -Format dd/MM/yyyy)
+  }
+
+  Describe -Name 'Testing mocks' -Fixture {
     Mock -CommandName Import-CSV -MockWith {
-      [PSCustomObject]@{
-        DriveLetter = 'C'
-        DateScanned = (Get-Date)
-      }
+      $global:mockedCSVSuccess
+    }
+    Mock -CommandName Get-Volume -MockWith {
+      $global:mockedVolume
+    }
+    Mock -CommandName Test-Path -MockWith {
+      return $true
+    }
+    It -name 'import-csv' -test {
+      (Import-Csv).driveletter | Should Be 'C'
+    }
+    It -name 'get-volume' -test {
+      (Get-Volume).driveletter  | Should Be 'C'
+    }
+    It -name 'test-path' -test {
+      Test-Path -Path C:\windows\temp\VIEDrives.csv  | Should Be 'true'
+    }
+  }
+  Describe -Name 'Testing if functions return correct objects' -Fixture {
+    Mock -CommandName Import-CSV -MockWith {
+      $global:mockedCSVSuccess
+    }
+    Mock -CommandName Get-Volume -MockWith {
+      $global:mockedVolume
+    }
+    Mock -CommandName Test-Path -MockWith {
+      return $true
     }
     It -name 'Get-TargetResource returns a hashtable' -test {
       Get-TargetResource -VIELocation $VIELocation | Should Be 'System.Collections.Hashtable'
@@ -24,31 +63,47 @@ InModuleScope -ModuleName xDSCSEPVIE -ScriptBlock {
     }
   }
 
-  Describe -Name 'Testing Get-TargetResource present/absent logic' -Fixture {
-    Mock -CommandName Test-Path -MockWith {
-      {
-        return $true
-      }
-    }
+  Describe -Name "Testing $($Global:DSCResourceName)\Get-TargetResource present/absent logic" -Fixture {
     Mock -CommandName Import-CSV -MockWith {
-      [PSCustomObject]@{
-        DriveLetter = 'C'
-        DateScanned = (Get-Date)
-      }
+      $global:mockedCSVSuccess
     }
-
+    Mock -CommandName Get-Volume -MockWith {
+      $global:mockedVolume
+    }
+    Mock -CommandName Test-Path -MockWith {
+      return $true
+    }
     It -name 'Get-TargetResource should return present' -test {
       (Get-TargetResource -VIELocation $VIELocation).Values | Should Be 'Present'
     }
     Mock -CommandName Import-CSV -MockWith {
-      [PSCustomObject]@{
-        DriveLetter = 'GG'
-        DateScanned = (Get-Date)
-      }
+      $global:mockedCSVFailure
     }
     It -name 'Get-TargetResource should return absent' -test {
       (Get-TargetResource -VIELocation $VIELocation).Values | Should Be 'Absent'
     }
-
+  }
+  
+  
+  
+  Describe -Name "Testing $($Global:DSCResourceName)\Test-TargetResource logic" -Fixture {
+    Mock -CommandName Import-CSV -MockWith {
+      $global:mockedCSVSuccess
+    }
+    Mock -CommandName Get-Volume -MockWith {
+      $global:mockedVolume
+    }
+    Mock -CommandName Test-Path -MockWith {
+      return $true
+    }
+    It -name 'Test-TargetResource should return true' -test {
+      Test-TargetResource -VIELocation $VIELocation -Ensure Present | Should Be 'True'
+    }
+    Mock -CommandName Import-CSV -MockWith {
+      $global:mockedCSVFailure
+    }    
+    It -name 'Test-TargetResource should return false' -test {
+      Test-TargetResource -VIELocation $VIELocation -Ensure Absent | Should Be 'True'
+    }
   }
 }
